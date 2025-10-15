abf1d50c5422f103d90d937bb58124ab17b2e6a2
anishumar <anishumar786@gmail.com>
2025-10-15 11:22:33 +0530

    Initial Commit

---
+++ StockPortfolio/StockPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPortfolioApp.swift
+//  StockPortfolio
+//
+//  Created by Anish Umar on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StockPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
